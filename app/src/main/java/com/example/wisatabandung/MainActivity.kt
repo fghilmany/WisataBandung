57e77dbc66ee4ec27965f6011ae58483c7d03af0
Faris Ghilmany <ceeros99@gmail.com>
2019-12-12 21:31:14 +0700

    splash screen dan desain login

---
+++ app/src/main/java/com/example/wisatabandung/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.wisatabandung
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var animSplash : Animation
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        animSplash = AnimationUtils.loadAnimation(this, R.anim.anim_splash)
+
+        iv_logo_splash.startAnimation(animSplash)
+
+        //intent otomatis setelah 2 detik (2000 ms)
+        Handler().postDelayed({
+            val i = Intent(this, LoginActivity::class.java)
+            startActivity(i)
+            finish()
+
+        },2000)
+    }
+}
