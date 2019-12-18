cb1900bb6808b68dc1eef064bf4583b89686b77d
Faris Ghilmany <ceeros99@gmail.com>
2019-12-18 23:04:41 +0700

    button fungtion , alert dialog and data

--- app/src/main/java/com/example/wisatabandung/activity/DetailDestination.kt
+++ app/src/main/java/com/example/wisatabandung/activity/DetailDestination.kt
@@ -1,18 +1,24 @@
 package com.example.wisatabandung.activity
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.Toolbar
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Toast
 import com.example.wisatabandung.R
 import com.example.wisatabandung.adapter.PagerAdapter
 import com.google.firebase.database.*
-import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_detail_destination.*
+import kotlinx.android.synthetic.main.dialog_buy_ticket.view.*
 import org.jetbrains.anko.startActivity
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+
+
 
 class DetailDestination : AppCompatActivity(), View.OnClickListener {
 
@@ -23,6 +29,16 @@ class DetailDestination : AppCompatActivity(), View.OnClickListener {
     private var username : String = ""
     private var id_destination : String = ""
     private var id_category : String = ""
+    private var name_destination : String = ""
+    private var name_user : String = ""
+
+    private var totalTicket : Int = 1
+    private var userBalance : Long = 10
+    private var totalCost : Long = 0
+    private var ticketPrice : Long = 0
+
+    private var bookDate : String = ""
+    private var expiredDate : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +49,16 @@ class DetailDestination : AppCompatActivity(), View.OnClickListener {
         initPager()
         initDataFirebase()
 
-        btn_buy_ticket.setOnClickListener(this)
 
+        tv_total_detail.setText(totalTicket.toString())
+
+
+        btn_buy_ticket.setOnClickListener(this)
+        btn_min.setOnClickListener(this)
+        btn_plus.setOnClickListener(this)
+
+        btn_min.animate().alpha(0F).setDuration(300).start()
+        btn_min.isEnabled = false
 
     }
 
@@ -42,19 +66,30 @@ class DetailDestination : AppCompatActivity(), View.OnClickListener {
         val intent = intent
         id_destination = intent.getStringExtra("id_destination")
         id_category = intent.getStringExtra("id_category")
-        Log.e("id ca",id_category)
-        Log.e("id de",id_destination)
 
-        ref = FirebaseDatabase.getInstance().getReference().child("destination").child(id_category).child(id_destination)
+
+        ref = FirebaseDatabase.getInstance().getReference()
         ref.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {
 
             }
 
             override fun onDataChange(p0: DataSnapshot) {
-                tv_name_destination.setText(p0.child("name").value.toString())
-                tv_price_detail.setText(p0.child("price").value.toString())
-                tv_detail_destination.setText(p0.child("description").value.toString())
+                val getDataDestination = p0.child("destination").child(id_category).child(id_destination)
+                val getDataUser = p0.child("user").child(username)
+
+                tv_name_destination.setText(getDataDestination.child("name").value.toString())
+                tv_price_detail.setText(getDataDestination.child("price").value.toString())
+                tv_detail_destination.setText(getDataDestination.child("description").value.toString())
+
+                name_destination = getDataDestination.child("name").value.toString()
+                name_user = getDataUser.child("name").value.toString()
+                userBalance = getDataUser.child("balance").value as Long
+                ticketPrice = getDataDestination.child("price").value as Long
+                totalCost = ticketPrice * totalTicket
+
+                Log.e("tot",totalCost.toString())
+                tv_total_cost.setText("$totalCost")
 
             }
 
@@ -64,11 +99,100 @@ class DetailDestination : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when(v!!.id){
             R.id.btn_buy_ticket->{
-                startActivity<SuccessBuyActivity>(
-                    "username" to username
-                )
+
+                bookAndExpDate()
+
+
+                val mDialogView = LayoutInflater.from(this).inflate(R.layout.dialog_buy_ticket,null)
+                val mBuilder = AlertDialog.Builder(this)
+                    .setView(mDialogView)
+                val mAlertDialog = mBuilder.show()
+
+                mDialogView.tv_destination_dialog.setText(name_destination)
+                mDialogView.tv_name_dialog.setText(name_user)
+                mDialogView.tv_total.setText("$totalCost")
+                mDialogView.tv_total_order.setText("$totalTicket")
+                mDialogView.tv_expired.setText("$expiredDate")
+                mDialogView.tv_date_order.setText("$bookDate")
+
+                mDialogView.btn_buy.setOnClickListener {
+                    mAlertDialog.dismiss()
+                    startActivity<SuccessBuyActivity>(
+                        "username" to username
+
+                    )
+                }
+
+                mDialogView.btn_cancel.setOnClickListener{
+                    mAlertDialog.dismiss()
+                }
+
+            }
+
+            R.id.btn_min -> {
+                totalTicket-=1
+                tv_total_detail.setText(totalTicket.toString())
+                if (totalTicket < 2){
+                    btn_min.animate().alpha(0F).setDuration(300).start()
+                    btn_min.isEnabled = false
+                }
+                totalCost = ticketPrice * totalTicket
+                tv_total_cost.setText("$totalCost")
+
+                if (totalCost <= userBalance){
+                    btn_plus.animate().translationY(0F).alpha(1F).setDuration(350).start()
+                    btn_plus.isEnabled = true
+
+                    btn_buy_ticket.isEnabled = true
+                    btn_buy_ticket.setBackgroundResource(R.drawable.bg_button_green_primary)
+
+                }
+
+            }
+
+            R.id.btn_plus ->{
+                totalTicket+=1
+                tv_total_detail.setText(totalTicket.toString())
+                if (totalTicket > 1){
+                    btn_min.animate().alpha(1F).setDuration(300).start()
+                    btn_min.isEnabled = true
+                }
+                totalCost = ticketPrice * totalTicket
+                tv_total_cost.setText("$totalCost")
+
+                if (totalCost > userBalance){
+                    btn_plus.animate().alpha(0F).setDuration(300).start()
+                    btn_plus.isEnabled = false
+
+                    btn_buy_ticket.isEnabled = false
+                    btn_buy_ticket.setBackgroundResource(R.drawable.btn_cancel)
+
+
+                }
+
             }
         }
+    }
+
+    private fun bookAndExpDate() {
+        val date = Date()
+        val formatter = SimpleDateFormat("dd-MM-yyyy")
+        val answer : String = formatter.format(date)
+
+        Log.e("jam",answer)
+        bookDate = answer
+
+        val c = Calendar.getInstance()
+        val cdt = "18-12-2019"
+        val date2 = formatter.parse(cdt)
+        c.time = date2
+        c.add(Calendar.DAY_OF_MONTH, 7)
+        val dateExp = formatter.format(c.time)
+
+
+        expiredDate = dateExp.toString()
+
+        Log.e("jam1",expiredDate)
     }
 
     private fun initToolabar() {
@@ -82,7 +206,8 @@ class DetailDestination : AppCompatActivity(), View.OnClickListener {
 
         toolbar.setNavigationOnClickListener {
             startActivity<ListDestinationActivity>(
-                "username" to username
+                "username" to username,
+                "id_category" to id_category
             )
             finish()
         }
