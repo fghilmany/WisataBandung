f8d378bbe062c0414e3a7e98f45e0bc64ce64f02
Faris Ghilmany <ceeros99@gmail.com>
2019-12-20 11:30:56 +0700

    done 0.1

--- app/src/main/java/com/example/wisatabandung/fragment/ProfilFragment.kt
+++ app/src/main/java/com/example/wisatabandung/fragment/ProfilFragment.kt
@@ -12,6 +12,15 @@ import com.google.firebase.database.*
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_profil.*
 import org.jetbrains.anko.support.v4.startActivity
+import android.content.Intent
+import android.support.v7.app.AlertDialog
+import android.util.Log
+import android.view.ContextThemeWrapper
+import com.example.wisatabandung.activity.MainActivity
+import kotlinx.android.synthetic.main.activity_detail_destination.view.*
+import kotlinx.android.synthetic.main.dialog_top_up.*
+import org.jetbrains.anko.db.INTEGER
+
 
 class ProfilFragment : Fragment(), View.OnClickListener {
 
@@ -33,6 +42,7 @@ class ProfilFragment : Fragment(), View.OnClickListener {
         initDataFirebase()
 
         tv_logout.setOnClickListener(this)
+        btn_top_up.setOnClickListener(this)
 
 
     }
@@ -61,8 +71,51 @@ class ProfilFragment : Fragment(), View.OnClickListener {
     override fun onClick(v: View?) {
         when(v!!.id){
             R.id.tv_logout -> {
-                startActivity<LoginActivity>()
+                val intent = Intent(activity!!.applicationContext, LoginActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                startActivity(intent)
                 activity!!.finish()
+            }
+
+            R.id.btn_top_up->{
+
+                val mDialogView = LayoutInflater.from(activity).inflate(R.layout.dialog_top_up,null)
+                val mBuilder = AlertDialog.Builder(ContextThemeWrapper(activity,R.style.myDialog))
+                    .setView(mDialogView)
+                val mAlertDialog = mBuilder.show()
+
+                mAlertDialog.btn_yes_topup.setOnClickListener{
+                    mAlertDialog.dismiss()
+                    ref = FirebaseDatabase.getInstance().getReference("user").child(username)
+                    ref.addListenerForSingleValueEvent(object:ValueEventListener{
+                        override fun onCancelled(p0: DatabaseError) {
+
+                        }
+
+                        override fun onDataChange(p0: DataSnapshot) {
+                            val valueTopUp = mAlertDialog.edt_top_up.text.toString().toInt()
+                            val valueBalance = p0.child("balance").value.toString().toInt()
+
+
+                            Log.e("topup",valueBalance.toString())
+
+
+                            p0.ref.child("balance").setValue(valueBalance+valueTopUp)
+                        }
+
+                    })
+                }
+
+                mAlertDialog.btn_cancel_topup.setOnClickListener{
+                    mAlertDialog.dismiss()
+                }
+
+
+
+
+
             }
         }
     }
